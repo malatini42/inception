e562424a06e9674db7042b87c948d3a739567db7
malatini42 <malatini@student.42.fr>
2022-01-03 23:37:39 +0100

    pb base de donnees

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,18 +1,21 @@
 
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+# Installation safe de mariadb 
+usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-	# Apply config
-	sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+# Apply config - modification du fichier de conf pour que la base de donnees 
+# soit accessible de l'exterieur
+sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
 	
-	# Config MariaDB with default bases
-	mysqladmin --wait=30 ping
-	 #then
-	 #	printf "Unable to reach mariadb\n"
-	 #	exit 1
-	 #fi
+# Config MariaDB with default bases
+mysqladmin --wait=30 ping
+#pkill mariadb
 
-	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
-	pkill mariadb
+# Execution du script de creation de la base de donnees pipe avec la commande mariadb ? 
+eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
 
-usr/bin/mysqld_safe --datadir=/var/lib/mysql
+#Arret du service une fois la base de donnee creee ?
+pkill mariadb
+
+#Lancement du daemon mysqld_safe ?
+#usr/bin/mysqld_safe --datadir=/var/lib/mysql
