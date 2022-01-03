725abf3ef6ea080651b7dcca0fd10d5bf9df09f7
malatini42 <malatini@student.42.fr>
2022-01-03 17:52:00 +0100

    sauvegarde wordpress

--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,16 +1,14 @@
 # Deuxieme fichier essentiel de configuration de wordpress
+sleep 5 
+
+# Correction du fichier www.conf pour que la configuration soit bien ok
 target="/etc/php7/php-fpm.d/www.conf"
 
-# Equivalent a ecrire en ligne de commande, connexion a la base de donnees
-grep -E "listen = 127.0.0.1" $target > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-	sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
-	echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
-	echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
-	echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
-	echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
-	sleep 5
-fi
+sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
+echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
+echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
+echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
+echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
 
 # Configuration du site wordpress
 wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
@@ -22,6 +20,6 @@ wp theme install twentysixteen --activate
 # Creation du user principal (voir deuxieme user)
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
 
-#force to stay in foreground and ingore daemonize option from configuration file
-#Si on ne le met pas on aura une 502 et on tournera en boucle sans arriver sur la page
+# Force to stay in foreground and ingore daemonize option from configuration file
+# Si on ne le met pas on aura une 502 et on tournera en boucle sans arriver sur la page
 php-fpm7 --nodaemonize
