27ffcd45a7a3435443c0683dcac72a2f333bf352
malatini42 <malatini@student.42.fr>
2022-01-03 22:52:00 +0100

    sauvegarde modif mariadb

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,6 +1,4 @@
-# A revoir 
-cat .setup 2> /dev/null
-if [ $? -ne 0 ]; then
+
 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
 	# Apply config
@@ -8,14 +6,13 @@ if [ $? -ne 0 ]; then
 	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
 	
 	# Config MariaDB with default bases
-	if ! mysqladmin --wait=30 ping; then
-		printf "Unable to reach mariadb\n"
-		exit 1
-	fi
+	mysqladmin --wait=30 ping
+	 #then
+	 #	printf "Unable to reach mariadb\n"
+	 #	exit 1
+	 #fi
 
 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
 	pkill mariadb
-	touch .setup
-fi
 
 usr/bin/mysqld_safe --datadir=/var/lib/mysql
