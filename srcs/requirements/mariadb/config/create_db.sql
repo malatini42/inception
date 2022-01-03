4b0d5cbb1d873b82840f5a70d329f6a90a08d949
malatini42 <malatini@student.42.fr>
2022-01-04 00:30:50 +0100

    fin de journee - mariadb a revoir

--- srcs/requirements/mariadb/config/create_db.sql
+++ srcs/requirements/mariadb/config/create_db.sql
@@ -1,20 +1,42 @@
 /** Suppression des user et bases de donnees crees par defau, necessaire pour une correction avec succes */
+-- DELETE FROM	mysql.user WHERE User='';
+-- DROP DATABASE test;
+-- DELETE FROM mysql.db WHERE Db='test';
+-- DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+-- /** Definition d'un mot de passe pour acceder a mysql **/
+-- /** SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD'); **/
+
+-- /** A l air de generer une erreur, a revoir **/
+-- SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
+
+-- /** Creation de la base de donnees **/
+-- CREATE DATABASE $MARIADB_DB;
+
+-- /** Creation des users **/
+-- /** CREATE USER 'root'@'localhost' IDENTIFIED by '$MARIADB_ROOT_PWD'; */
+-- CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
+
+-- /** Dont de tous les privileges a cet utilisateur **/
+-- GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO $MARIADB_USER@'%';
+-- /** GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO 'root'@'localhost'; */
+
+-- /** Le flush est necessaire pour que les privileges soient a jour **/
+-- FLUSH PRIVILEGES;
+
 DELETE FROM	mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-/** Definition d'un mot de passe pour acceder a mysql **/
+
+--  set root pwd
 SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MARIADB_ROOT_PWD');
 
-/** Creation de la base de donnees **/
+--  create wordpress assets
 CREATE DATABASE $MARIADB_DB;
-
-/** Creation du premier utilisateur, voir deuxieme **/
 CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PWD';
-
-/** Dont de tous les privileges a cet utilisateur **/
 GRANT ALL PRIVILEGES ON $MARIADB_DB.* TO $MARIADB_USER@'%';
 
-/** Le flush est necessaire pour que les privileges soient mis a jour **/
+--  take effects
 FLUSH PRIVILEGES;
--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,21 +1,42 @@
 
-# Installation safe de mariadb 
-usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+# # Installation safe de mariadb 
+# usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-# Apply config - modification du fichier de conf pour que la base de donnees 
-# soit accessible de l'exterieur
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+# # Apply config - modification du fichier de conf pour que la base de donnees 
+# # soit accessible de l'exterieur
+# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+
 	
-# Config MariaDB with default bases
-mysqladmin --wait=30 ping
-#pkill mariadb
+# # Config MariaDB with default bases
+# mysqladmin --wait=30 ping
 
-# Execution du script de creation de la base de donnees pipe avec la commande mariadb ? 
-eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+# # Execution du script de creation de la base de donnees pipe avec la commande mariadb ? 
+# eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
 
-#Arret du service une fois la base de donnee creee ?
-pkill mariadb
+# #Arret du service une fois la base de donnee creee ?
+# pkill mariadb
 
-#Lancement du daemon mysqld_safe ?
-#usr/bin/mysqld_safe --datadir=/var/lib/mysql
+# #Lancement du daemon mysqld_safe ?
+# usr/bin/mysqld_safe --datadir=/var/lib/mysql
+
+cat .setup 2> /dev/null
+if [ $? -ne 0 ]; then
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+	# Apply config
+	sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+	
+	# Config MariaDB with default bases
+	if ! mysqladmin --wait=30 ping; then
+		printf "Unable to reach mariadb\n"
+		exit 1
+	fi
+
+	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
+	pkill mariadb
+	touch .setup
+fi
+
+usr/bin/mysqld_safe --datadir=/var/lib/mysql
