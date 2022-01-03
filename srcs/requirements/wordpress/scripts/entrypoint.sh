50c44334731b10e4f3eb7145e9e3483381518fc0
malatini42 <malatini@student.42.fr>
2022-01-03 16:36:32 +0100

    save

--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,4 +1,7 @@
+# Deuxieme fichier essentiel de configuration de wordpress
 target="/etc/php7/php-fpm.d/www.conf"
+
+# Equivalent a ecrire en ligne de commande, connexion a la base de donnees
 grep -E "listen = 127.0.0.1" $target > /dev/null 2>&1
 if [ $? -eq 0 ]; then
 	sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
@@ -6,24 +9,21 @@ if [ $? -eq 0 ]; then
 	echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
 	echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
 	echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
+	sleep 5
 fi
 
-if [ ! -f "wp-config.php" ]; then
-	cp /config/wp-config ./wp-config.php
+#cp /config/wp-config ./wp-config.php
 
-	sleep 5;
-	if ! mysqladmin -h $MARIADB_HOST -u $MARIADB_USER \
-		--password=$MARIADB_PWD --wait=60 ping > /dev/null; then
-		printf "MySQL is not available.\n"
-		exit 1
-	fi
-	wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
-		--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
+# Configuration du site wordpress
+wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
+	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 
-	wp plugin update --all
+# Installation de notre theme et "activation"
+wp theme install twentysixteen --activate
 
-	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
+# Creation du user principal (voir deuxieme user)
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
 
-fi
-
+#force to stay in foreground and ingore daemonize option from configuration file
+#Si on ne le met pas on aura une 502 et on tournera en boucle sans arriver sur la page
 php-fpm7 --nodaemonize
