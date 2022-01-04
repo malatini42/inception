0ebe9d14783ff9ab23a40cefa1b2d4fe06e08f42
malatini42 <malatini@student.42.fr>
2022-01-04 09:56:58 +0100

    reprise

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -20,8 +20,8 @@
 # #Lancement du daemon mysqld_safe ?
 # usr/bin/mysqld_safe --datadir=/var/lib/mysql
 
-cat .setup 2> /dev/null
-if [ $? -ne 0 ]; then
+#cat .setup 2> /dev/null
+#if [ $? -ne 0 ]; then
 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
 	# Apply config
@@ -29,14 +29,15 @@ if [ $? -ne 0 ]; then
 	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
 	
 	# Config MariaDB with default bases
-	if ! mysqladmin --wait=30 ping; then
-		printf "Unable to reach mariadb\n"
-		exit 1
-	fi
+	mysqladmin --wait=30 ping; 
+	#then
+	#	printf "Unable to reach mariadb\n"
+	#	exit 1
+	#fi
 
 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
 	pkill mariadb
-	touch .setup
-fi
+	#touch .setup
+#fi
 
 usr/bin/mysqld_safe --datadir=/var/lib/mysql
