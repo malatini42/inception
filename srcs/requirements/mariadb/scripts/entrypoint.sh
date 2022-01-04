3974dfcc1101fb3c2063bc21314e36787ecbfbaa
malatini42 <malatini@student.42.fr>
2022-01-04 15:54:15 +0100

    modif mariadb-server.cnf

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,33 +1,12 @@
-
-# # Installation safe de mariadb 
-# usr/bin/mysqld_safe --datadir=/var/lib/mysql &
-
-# # Apply config - modification du fichier de conf pour que la base de donnees 
-# # soit accessible de l'exterieur
-# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
-
-	
-# # Config MariaDB with default bases
-# mysqladmin --wait=30 ping
-
-# # Execution du script de creation de la base de donnees pipe avec la commande mariadb ? 
-# eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
-
-# #Arret du service une fois la base de donnee creee ?
-# pkill mariadb
-
-# #Lancement du daemon mysqld_safe ?
-# usr/bin/mysqld_safe --datadir=/var/lib/mysql
-
+# Si la base de donnees a deja ete creee on ne rentrera pas dans le premier if
 cat .setup 2> /dev/null
 if [ $? -ne 0 ]; then
 	# https://dev.mysql.com/doc/refman/8.0/en/mysqld-safe.html
 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
 	# Apply config
-	sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+	# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+	# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
 	
 	# Config MariaDB with default bases
 	if ! mysqladmin --wait=30 ping; #then
@@ -41,5 +20,5 @@ if [ $? -ne 0 ]; then
 	touch .setup
 fi
 
-#Sera utilise pour que le container wordpress puisse proceder a la configuration
+# Va permettre de lancer le daemon
 usr/bin/mysqld_safe --datadir=/var/lib/mysql
