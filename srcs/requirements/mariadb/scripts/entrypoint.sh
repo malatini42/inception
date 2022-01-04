340ac8f186ca4e97fb6983947b2b707965416604
malatini42 <malatini@student.42.fr>
2022-01-04 12:54:10 +0100

    script entrypoint doit rester inchange

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -20,8 +20,8 @@
 # #Lancement du daemon mysqld_safe ?
 # usr/bin/mysqld_safe --datadir=/var/lib/mysql
 
-#cat .setup 2> /dev/null
-#if [ $? -ne 0 ]; then
+cat .setup 2> /dev/null
+if [ $? -ne 0 ]; then
 	# https://dev.mysql.com/doc/refman/8.0/en/mysqld-safe.html
 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
@@ -30,16 +30,16 @@
 	sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
 	
 	# Config MariaDB with default bases
-	if ! mysqladmin --wait=30 ping; then
-	#then
+	if ! mysqladmin --wait=30 ping; #then
+	then
 		printf "Unable to reach mariadb\n"
 		exit 1
 	fi
 
 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
 	pkill mariadb
-	#touch .setup
-#fi
+	touch .setup
+fi
 
 #Sera utilise pour que le container wordpress puisse proceder a la configuration
 usr/bin/mysqld_safe --datadir=/var/lib/mysql
