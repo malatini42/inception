0697e7795926088c8af3c5d5f7dfde637f500a4e
malatini42 <malatini@student.42.fr>
2022-01-04 16:54:43 +0100

    ajout des articles

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -2,23 +2,25 @@
 cat .setup 2> /dev/null
 if [ $? -ne 0 ]; then
 	# https://dev.mysql.com/doc/refman/8.0/en/mysqld-safe.html
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
-
-	# Apply config
-	# sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
-	# sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0\nport=3306|g" /etc/my.cnf.d/mariadb-server.cnf
+	# Le & va etre utilise pour effectuer une "modification sur le serveur MySQL" - mysql driver avec des options
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql & #--datadir=/var/lib/mysql
 	
 	# Config MariaDB with default bases
-	if ! mysqladmin --wait=30 ping; #then
-	then
-		printf "Unable to reach mariadb\n"
-		exit 1
-	fi
+	# if ! mysqladmin --wait=30 ping; #then
+	# then
+	# 	printf "Unable to reach mariadb\n"
+	# 	exit 1
+	# fi
+
+	# Il est necessaire d'attendre que la base de donnee soit bien accessible, mysql lance
+	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
+    	sleep 1
+	done
 
 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
-	pkill mariadb
+	# pkill mariadb
 	touch .setup
 fi
 
-# Va permettre de lancer le daemon
-usr/bin/mysqld_safe --datadir=/var/lib/mysql
+# Va permettre de lancer le serveur de facon securisee
+usr/bin/mysqld_safe #--datadir=/var/lib/mysql
--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,4 +1,3 @@
-# Deuxieme fichier essentiel de configuration de wordpress
 sleep 5 
 
 # Correction du fichier www.conf pour que la configuration soit bien ok
@@ -19,6 +18,9 @@ wp theme install twentysixteen --activate
 
 # Creation du user principal (voir deuxieme user)
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
+
+# Creation d'un article pour l'example
+wp post generate --count=5 --post_title="malatini"
 
 # Force to stay in foreground and ingore daemonize option from configuration file
 # Si on ne le met pas on aura une 502 et on tournera en boucle sans arriver sur la page
