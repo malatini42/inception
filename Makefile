769f730eaaa19f2423de8c7a90bf2e24b5dc9d34
malatini42 <malatini@student.42.fr>
2022-01-04 18:21:50 +0100

    modif makefile

--- Makefile
+++ Makefile
@@ -3,28 +3,47 @@ DOCKER			= docker
 COMPOSE 		= cd srcs/ && docker-compose
 DATA_PATH 		= /home/malatini/data
 
+# Handling DNS issues, build services and start containers
 all		:	build
 			sudo mkdir -p $(DATA_PATH)
+# Seront utilises pour les volumes (les donnees seront ici sur l'hote)
 			sudo mkdir -p $(DATA_PATH)/wordpress
 			sudo mkdir -p $(DATA_PATH)/database
+# Va permettre d'acceder a https://malatini.42.fr en local
 			sudo chmod 777 /etc/hosts
 			sudo echo "127.0.0.1 malatini.42.fr" >> /etc/hosts
 			$(COMPOSE) up -d
 
+#build or rebuild services
 build	:
 			$(COMPOSE) build
 
+# Creates and start containers
+up:
+			${COMPOSE} up -d 
+
+# Stops containers and removes containers, networks, volumes, and images created by up
 down	:
 			$(COMPOSE) down
 
+# Pause containers
+pause:
+			$(COMPOSE) pause
+# Unpause containers 
+unpause:
+			$(COMPOSE) unpause
+
+# down and make sure every containers are deleted
 clean	:
 			$(COMPOSE) down -v --rmi all --remove-orphans
 
+# cleans and makes sure every volumes, networks and image are deleted
 fclean	:	clean
 			$(DOCKER) system prune --volumes --all --force
 			sudo rm -rf $(DATA_PATH)
 			$(DOCKER) network prune --force
-			$(DOCKER) volume prune --force
+			echo docker volume rm $(docker volume ls -q)
 			$(DOCKER) image prune --force
+# $(DOCKER) volume prune --force
 
 re		:	fclean all
--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,27 +1,56 @@
 sleep 5 
 
-# Correction du fichier www.conf pour que la configuration soit bien ok
-target="/etc/php7/php-fpm.d/www.conf"
+# # Correction du fichier www.conf pour que la configuration soit bien ok
+# target="/etc/php7/php-fpm.d/www.conf"
 
-sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
-echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
-echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
-echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
-echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
+# sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
+# echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
+# echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
+# echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
+# echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
 
-# Configuration du site wordpress
-wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
-	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
+# # Configuration du site wordpress
+# wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
+# 	--admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
 
-# Installation de notre theme et "activation"
-wp theme install twentysixteen --activate
+# # Installation de notre theme et "activation"
+# wp theme install twentysixteen --activate
 
-# Creation du user principal (voir deuxieme user)
-wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
+# # Creation du user principal (voir deuxieme user)
+# wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
 
-# Creation d'un article pour l'example
-wp post generate --count=5 --post_title="malatini"
+# # Creation d'un article pour l'example
+# wp post generate --count=5 --post_title="malatini"
 
 # Force to stay in foreground and ingore daemonize option from configuration file
 # Si on ne le met pas on aura une 502 et on tournera en boucle sans arriver sur la page
+
+# cat .setupwordpress 2> /dev/null
+# if [ $? -ne 0 ]; then
+    # Correction du fichier www.conf pour que la configuration soit bien ok
+    target="/etc/php7/php-fpm.d/www.conf"
+
+    sed -i "s|.*listen = 127.0.0.1.*|listen = 9000|g" $target
+    echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> $target
+    echo "env[MARIADB_USER] = \$MARIADB_USER" >> $target
+    echo "env[MARIADB_PWD] = \$MARIADB_PWD" >> $target
+    echo "env[MARIADB_DB] = \$MARIADB_DB" >> $target
+
+    # Configuration du site wordpress
+    wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" \
+        --admin_password="$WP_ADMIN_PWD" --admin_email="$WP_ADMIN_EMAIL" --skip-email
+
+    # Installation de notre theme et "activation"
+    wp theme install twentysixteen --activate
+
+    # Creation du user principal (voir deuxieme user)
+    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PWD
+
+    # Creation d'un article pour l'example
+    wp post generate --count=5 --post_title="malatini"
+
+    touch .setupwordpress
+# fi
+
+# On a besoin de ca pour faire tourner wordpress mais aussi pour que le container keep running
 php-fpm7 --nodaemonize
