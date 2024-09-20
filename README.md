7dc20fe7edaa606ad108c94e43f440afcef9f072
Mahaut Latinis <malatini@student.42.fr>
2024-09-20 11:29:54 +0200

    clean: edit mac (#7)

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ inception - 2022 (Common Core).
 - Actual Status : validated (07/01/2022)
 
 I updated the project since its validation.
+The version is currently working on my own macbook without any VM.
+
 <img width="1433" alt="inception" src="https://github.com/malatinipro/inception/assets/77189438/fb84c467-d88d-478e-ae49-0f9d093adeaf">
 <img width="1437" alt="comment" src="https://github.com/malatinipro/inception/assets/77189438/1d3ec130-cf06-4fd7-bb4f-e5f6936e7a20">
 <img width="1439" alt="images" src="https://github.com/malatinipro/inception/assets/77189438/73f09200-2472-415c-96c3-8cd2fea726f5">
+
 Thanks @raccoman for your great example repository.
-[ Deprecated ]
-![Alt text](/inception.png?raw=true "Inception")
+![Alt text](/inception.png?raw=true 'Inception')
--- srcs/requirements/wordpress/script/configure.sh
+++ srcs/requirements/wordpress/script/configure.sh
@@ -5,12 +5,10 @@ while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD $WP_DATABASE
 done
 
 if [ ! -f "/var/www/html/index.html" ]; then
-    wp core download
+    wp core download --locale=fr_FR
     wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USR --dbpass=$WP_DATABASE_PWD --dbhost=$MYSQL_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci"
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+    wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
     wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD
     wp theme install inspiro --activate
-    wp plugin update --all 
 fi
-
 php-fpm7 --nodaemonize
